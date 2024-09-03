4752c2c08c2419b36230d7ca351ff8aa10b1c3f7
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-03 05:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let audios=document.querySelectorAll("audio")
+let stop =document.getElementsByClassName(".stop")
+let btns=document.querySelectorAll(".btn")
+btns.forEach((value)=>{
+	value.addEventListener("click",()=>{
+		audios.forEach((aud)=>{
+			if(btns.indexof(value)==audios.indexof(aud)){
+				aud.play();
+			}else{
+				for (let index = 0; index < audios.length; index++) {
+					if(btns.indexof(value)!=audios.indexof(aud)){
+	                 audios[index].pause();
+					}
+			}
+		})
+	})
+})
+stop.addEventlistener("click",()=>{
+for (let index = 0; index < audios.length; index++) {
+	audios[index].pause();
+	
+}
+	});
